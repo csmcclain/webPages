e2a8ab0f960ad0b85b9e81a5d164b0ac184ea24e
csmcclain <christopher.mcclain@icloud.com>
2020-02-19 11:18:48 -0700

    added api

--- cp2.csmcclain.com/script.js
+++ cp2.csmcclain.com/script.js
@@ -1,12 +1,7 @@
-function append(parent, el)
-{
-    return parent.appendChild(el);
-}
-
 
 function getCharacters(e)
 {
-    debugger;
+    
     e.preventDefault();
     //now lets get them values
     let something = document.getElementById('houseSelector');
@@ -17,49 +12,26 @@ function getCharacters(e)
     let ministryOfMagics = document.getElementById('ministryOfMagic').checked;
 
     //setup url
-    const key = '$2a$10$tE9Q/PpSuP7rQLFkrB2IOOcl.0ptM34qLwotYCBjL/p9DIL.o4pMK';
-    const url = new URL('https://www.potterapi.com/v1/characters/'), params = {key: key, house: hogwartsHouse.value}
-    
+    let url = 'https://www.potterapi.com/v1/characters?key=$2a$10$e2vvEKONEi4fdC.ROuFEMOdwu4xG3VEjR.S9URLPsNero/Fanpasy';
+    debugger;
     console.log(url);
-    //Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
-
-    fetch(url, {key: key})
-  .then((resp) => resp.json()) // Transform the data into json
-  .then(function(data) {
-  // Get the results
-  let characters = data;
-  // console.log(characters);
-  //Map through the results
-  return characters.map(function(character) {
-    // Create the li's
-    let li = createNode('li');
-    // Make the HTML of our li the character's name and blood status
-    li.innerHTML = `${character.name}, ${character.bloodStatus}`;
-    // Append the li's to the ul
-    append(ul, li);
-  })
-})
-.catch(function(error) {
-  // Error handling
-  let p = "There's an error with the API.";
-  append(body, p);
-});
-    
-
-
-
+    fetch(url)
+    .then(function(response) 
+    {
+      console.log(response);
+      // make sure the request was successful
+      if (response.status != 200) {
+        return {
+          text: "Error calling the Numbers API service: " + response.statusText
+        }
+      }
+      return response.json();
+    }).then(function(json) 
+    {
+      let results = "";
+      results += "<h1>";
+          
+    });
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 document.getElementById('get').addEventListener('click', getCharacters)
